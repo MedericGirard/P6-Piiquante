196e6872f044e86f5a1d70e405eba42c3298f241
Médéric Girard <girard.mederic@gmail.com>
2022-10-03 13:59:23 +0200

    FIx modif sauce

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -49,14 +49,15 @@ exports.getOneSauce = (req, res, next) => {
   };
 
   exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { 
-      ...req.body,
-       _id: req.params.id ,
-       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    })
-        .then(() => res.status(200).json({message: 'Sauce modifiée'}))
-        .catch(error => res.status(400).json({ error }));
-  };
+    const sauceObject = req.file ?
+    {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id} , {...sauceObject, _id: req.params.id})
+    .then(()=> res.status(200).json({ message: 'Sauce modifiée'}))
+    .catch(()=> res.status(400).json({ error}))
+};
   
   exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id})
